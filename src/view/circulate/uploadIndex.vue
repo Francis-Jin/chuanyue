06bdd8a1993fc90b552cf609b464372898b8786c
Francis-Jin <jasfunction@163.com>
2019-09-11 13:45:27 +0800

    上传页面初始化JSSDK

--- src/view/circulate/uploadIndex.vue
+++ src/view/circulate/uploadIndex.vue
@@ -64,6 +64,8 @@ export default {
     document.title = '拍照上传'
     this.getUrserInfoData()
     // this.simpleGetChildresDetails()
+    /** 初始哈jssdk. */
+    this.initJssdk()
   },
   methods: {
     toTarget (index) {
@@ -150,7 +152,41 @@ export default {
           sessionStorage.setItem('userInfo', JSON.stringify(userInfo))
         }
       })
-    }
+    },
+    /* eslint-disable */
+      /** 初始哈jssdk. */
+      initJssdk () {
+          let url = window.location.href.split('#')[0]
+          let parm = this.$Qs.stringify({
+              url: url
+          })
+          //  路由全局 注入config
+          this.$api.getJsSingle(parm).then(res => {
+              if (res.data.code * 1 === 200) {
+                  let data = res.data.data
+                  wx.config({
+                      // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                      debug: false,
+                      // 必填，公众号的唯一标识
+                      appId: data.appid,
+                      // 必填，生成签名的时间戳
+                      timestamp: data.timestamp,
+                      // 必填，生成签名的随机串
+                      nonceStr: data.nonceStr,
+                      // 必填，签名
+                      signature: data.signature,
+                      // 必填，需要使用的JS接口列表，所有JS接口列表
+                      jsApiList: ['scanQRCode']
+                  })
+                  wx.ready(function () {
+
+                  })
+                  wx.error(function (res) {
+                      // alert(JSON.stringify(res))
+                  })
+              }
+          })
+      },
   }
 }
 </script>

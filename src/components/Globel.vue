c18dcbc7dabb24713a8702cd71429c6405e5a70e
king <jasfunction@163.com>
2019-09-29 01:37:33 +0800

    样式修改

--- src/components/Globel.vue
+++ src/components/Globel.vue
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 
-// const BEFORE_IMG_URL = 'http://cy.zhiyanginfo.top:8081' // 正式图片前缀
-const BEFORE_IMG_URL = 'http://houtan.zhiyanginfo.top:8081' // 测试图片前缀
+const BEFORE_IMG_URL = 'http://cy.zhiyanginfo.top:8081' // 正式图片前缀
+// const BEFORE_IMG_URL = 'http://houtan.zhiyanginfo.top:8081' // 测试图片前缀
 // const BEFORE_IMG_URL = 'http://zhuyb.4kb.cn/' // 本地图片前缀
 const LISTS_LIMIT = 20
 export default {
